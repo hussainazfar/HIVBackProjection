320c223689a2918e9e233275b14d38bc517b0846
James Jansson <jamesjansson@gmail.com>
2014-10-13 19:14:09 +1100

    Generating CD4 counts

--- OptimiseTestingRateToCD4.m
+++ OptimiseTestingRateToCD4.m
@@ -1,29 +1,34 @@
 function [ChosenCD4Count, PEstimate]=OptimiseTestingRateToCD4(RealTestingCD4, Pxi )
 
     ExpectedOutput=hist(RealTestingCD4, 50:100:1950);
-    NumberOfCD4Counts=size(1, RealTestingCD4);
+    [~, NumberOfCD4Counts]=size(1, RealTestingCD4);
     FunctionPointer=@GenerateCD4Count;
     % [ResultsForOPtimisation, OtherOutput]=GenerateCD4Count(TestingRateParameters, FunctionInput);
     % TestingRateParameters(1)= AnnualTestingRate
     % TestingRateParameters(1)= in the equation 
     % OtherOutput= raw CD4 counts
     
-    OptimisationSettings.ErrorFunction=true;
+    OptimisationSettings=[];
     
+    Pxi.SimulatedPopSize=NumberOfCD4Counts;
+    FunctionInput=Pxi;
     
+    ParameterBounds=[0, 1; 0, 1];
     
     % Determine a likely testing rate 
-    StochasticOptimise(FunctionPointer, FunctionInput, ParameterBounds, ExpectedOutput, OptimisationSettings)
+    [OptimisedParameters, ~]=StochasticOptimise(FunctionPointer, FunctionInput, ParameterBounds, ExpectedOutput, OptimisationSettings)
 
     % Rerun the best testing rate, match CD4 counts with
-    SimulatedPopSize=10000;
+    Pxi.SimulatedPopSize=10000;
     
-    []=GenerateCD4Count();
+    [~, Data]=GenerateCD4Count(OptimisedParameters, Pxi);
     
     % Choose nearest neighbours
     NumberOfPeople=0;
     for ThisCD4=RealTestingCD4
         NumberOfPeople=NumberOfPeople+1;
+        %Find CD4 counts that are within 
+        Data.CD4
         
     end
     
