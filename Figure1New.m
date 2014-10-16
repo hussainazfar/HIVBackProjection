7b9ad63bca80cf478b89ad44f5d6144c84629d15
JamesJansson <jamesjansson@gmail.com>
2014-10-16 19:04:43 +1100

    Creating a new figure 1

--- Figure1New.m
+++ Figure1New.m
@@ -7,16 +7,27 @@
     
     Pxi.CD4Decline=mean(Px.CD4DeclineVec); % select a sample of this parameter
     Pxi.FractionalDeclineToRebound=mean(Px.FractionalDeclineToReboundVec); % select a sample of this parameter
-    Pxi.SimulatedPopSize=1000000;
+    Pxi.SimulatedPopSize=100000;
     
-    TestingParameters=[0.1, 0, 0];%low, flat testing rate
+    TestingParameters=[0.1, 0, 0];%low, flat testing rate should not bias towards high starting CD4s
 
 [CD4CountHistogram, Data]=GenerateCD4Count(TestingParameters, Pxi);
 
-
+Count=0;
 for Time=0:0.1:20
-    Data.Time
+    Count=Count+1;
+    IndexInStep=Time<=Data.Time & Data.Time<Time+0.1;
     
-    Data.CD4
-    
+    TestingCD4=Data.CD4(IndexInStep);
+     t(Count)=Time;
+     CD4Median(Count)=median(TestingCD4);
+     CD4U95(Count)=prctile(TestingCD4, 97.5);
+     CD4L95(Count)=prctile(TestingCD4, 2.5);
 end
+
+clf;
+hold on;
+plot(t, CD4Median);
+plot(t,CD4U95);
+plot(t,CD4L95);
+hold off;
