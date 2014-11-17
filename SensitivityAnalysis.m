e63588e8456b4d03274e7af2760ad4c4e2e1b6f1
James Jansson <jamesjansson@gmail.com>
2014-11-17 15:22:54 +1100

    Further sensitivity

--- SensitivityAnalysis.m
+++ SensitivityAnalysis.m
@@ -19,7 +19,7 @@ print('-dpng ','-r300','ResultsPlots/Appendix Max inc v first infection.png')
 
 
 
-plot(Px.FirstInfectionDateVec,YearVectorLabel(MaxIncidenceYearIndex))
+%plot(Px.FirstInfectionDateVec,YearVectorLabel(MaxIncidenceYearIndex))
 
 % Compare post primary CD4 infection to time between infection and diagnosis
 plot(Px.BaselineCD4MedianVec,TimeSinceInfectionMatrix(end).Median, '.')
@@ -34,7 +34,10 @@ lsline
 print('-dpng ','-r300','ResultsPlots/Appendix post-primary CD4 v time until diag.png')
 
 
-% Compare post primary CD4 infection to time between infection and diagnosis
+
+
+
+% Compare CD4 decline to time between infection and diagnosis
 plot(Px.SQRCD4DeclineVec,TimeSinceInfectionMatrix(end).Median, '.')
 
 xlabel('Median sqr root CD4 decline','fontsize', 22);
@@ -45,3 +48,30 @@ set(gca, 'fontsize', 18)
 box off;
 lsline
 print('-dpng ','-r300','ResultsPlots/Appendix sqr decline v time until diag.png')
+
+
+
+% Compare CD4 decline to incidence in final year
+plot(Px.SQRCD4DeclineVec,DistributionTotal(:, end), '.')
+
+xlabel('Median sqr root CD4 decline','fontsize', 22);
+ylabel('Incidence in final year of simulation','fontsize', 22);
+set(gca,'Color',[1.0 1.0 1.0]);
+set(gcf,'Color',[1.0 1.0 1.0]);%makes the grey border white
+set(gca, 'fontsize', 18)
+box off;
+lsline
+print('-dpng ','-r300','ResultsPlots/Appendix sqr decline v incidence in final year.png')
+
+
+% Compare baseline post-primary infection CD4 to incidence in final year
+plot(Px.BaselineCD4MedianVec,DistributionTotal(:, end), '.')
+
+xlabel('Median CD4 following primary infection','fontsize', 22);
+ylabel('Incidence in final year of simulation','fontsize', 22);
+set(gca,'Color',[1.0 1.0 1.0]);
+set(gcf,'Color',[1.0 1.0 1.0]);%makes the grey border white
+set(gca, 'fontsize', 18)
+box off;
+lsline
+print('-dpng ','-r300','ResultsPlots/Appendix Baseline CD4 v incidence in final year.png')
