159243e9d143d0524f4f0a5d96fa9c3d19f8c37d
James Jansson <jamesjansson@gmail.com>
2014-11-18 20:49:53 +1100

    Plotting R values on charts

--- SensitivityAnalysis.m
+++ SensitivityAnalysis.m
@@ -65,6 +65,21 @@ print('-dpng ','-r300','ResultsPlots/Appendix sqr decline v incidence in final y
 % Compare baseline post-primary infection CD4 to incidence in final year
 plot(Px.BaselineCD4MedianVec,DistributionTotal(:, end), '.', 'MarkerSize', 20)
 ylim([0, inf])
+%Display R values and P values
+[r, rp]=corrcoef(Px.BaselineCD4MedianVec, DistributionTotal(:, end));
+rstring=['r=' num2str(r(1,2))];
+if rp(1, 2)<0.001
+    pstring='p<0.001';
+else
+    pstring=['p=', num2str(rp(1, 2))];
+end
+yrange=get(gca,'ylim');
+xrange=get(gca,'xlim');
+htexthandle=text(xrange(1)+0.05*(xrange(2)-xrange(1)),yrange(1)+0.15*(yrange(2)-yrange(1)),{rstring, pstring} );
+set(htexthandle, 'FontSize', 18);
+
+
+
 xlabel('Median CD4 following primary infection','fontsize', 22);
 ylabel('Incidence in final year of simulation','fontsize', 22);
 set(gca,'Color',[1.0 1.0 1.0]);
@@ -72,4 +87,9 @@ set(gcf,'Color',[1.0 1.0 1.0]);%makes the grey border white
 set(gca, 'fontsize', 18)
 box off;
 lsline
+
+
+
+
+
 print('-dpng ','-r300','ResultsPlots/Appendix Baseline CD4 v incidence in final year.png')
