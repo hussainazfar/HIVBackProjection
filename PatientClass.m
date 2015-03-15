1527347593bb68298bc92477a44af034a1a36933
hussainazfar <hussainazfar@gmail.com>
2015-03-15 19:09:35 +1100

    Creation of Patient Object Update
    Lines in code were updated to conform to changes in previous function
    definitions/lexicon/variable naming

--- PatientClass.m
+++ PatientClass.m
@@ -1,48 +1,13 @@
-classdef PatientClass %< handle  
-   properties %(SetAccess='private')
-%       HIVStatus;
-%       AIDSStatus;
-%       Alive;
-      ID;
-      YearBirth;
-      DOB;
-      Sex;
-      DateOfDiagnosis;%text
-      DateOfDiagnosisContinuous;%decimal
-      YearOfDiagnosis;%rounded
-      TimeFromInfectionToDiagnosis;%used to store an array of the expected distribution of time since infection
-%       InfectionDate;%indicates the actual date of infection selected for this individual 
-      InfectionDateDistribution;%indicates the actual date of infection selected for this individual
-      YearOfAIDSDiagnosis;
-      %YearTable;%%%%%%%%%%%DEPRECIATED
-%       YearFirstTreatment;%the year that patient goes on to treatment for the first time
-      IndigenousStatus;
-      PostcodeAtDiagnosis;
-      StateAtDiagnosis;
-      SLAAtDiagnosis;
-      SRAtDiagnosis;
-      SSDAtDiagnosis;
-      
-      %Variables that indicate recent diagnosis
-      DateIll;
-      DateIndetWesternBlot;
-      DateLastNegative;
-      
-      
-%       PopulationCategory;%Category values
-%       % GroupCode.MSM=1;
-%       % GroupCode.HeteroOnlyMale=2;
-%       % GroupCode.Female=3;
-%       % GroupCode.MSMIDU=4;
-%       % GroupCode.HeteroOnlyMaleIDU=5;
-%       % GroupCode.FemaleIDU=6;
-      
-
-      
-      CD4CountAtDiagnosis;
-      ExposureRoute;
-      
-            % Code Exposure category
+classdef PatientClass
+    %Create a Class Definition for all Patients in LineMatrixData
+    %Code Sex
+            % 1    Male
+            % 2    Female
+            % 3    Transgender
+    %Code Recent infection
+            % 0    No
+            % 1    Yes
+    %Code Exposure  - Exposure Route category
             % MSM	1
             % MSM IDU	2
             % Bisexual male	3
@@ -56,7 +21,7 @@ classdef PatientClass %< handle
             % MTCT	11
             % Unknown	12
             
-            % OLD exposure coding
+    %OLD Code Exposure - Exposure Route category
             % 1    Male homosexual/bisexual contact
             % 2    Male homosexual/bisexual contact and IDU
             % 3    IDU + Heterosexual
@@ -73,100 +38,89 @@ classdef PatientClass %< handle
             % 14   Health care setting
             % 15   Mother with/at risk for HIV infection
             % 16   Other/undetermined
-            % 
-            % Code Sex
-            % 1    Male
-            % 2    Female
-            % 3    Transgender
-            % 
-            % Code Recent infection
-            % 0    No
-            % 1    Yes
-
+    %Group Codes           
+            % GroupCode.MSM=1;       
+            % GroupCode.HeteroOnlyMale=2;
+            % GroupCode.Female=3;
+            % GroupCode.MSMIDU=4;
+            % GroupCode.HeteroOnlyMaleIDU=5;
+            % GroupCode.FemaleIDU=6;
+    
+    properties                                                              %(SetAccess = 'private')
+      %HIVStatus;
+      %AIDSStatus;
+      %Alive;
+      ID;
+      YearBirth;
+      DOB;
+      Sex;
+      DateOfDiagnosis;                                                      %text
+      DateOfDiagnosisContinuous;                                            %decimal
+      YearOfDiagnosis;                                                      %rounded
+      TimeFromInfectionToDiagnosis;                                         %used to store an array of the expected distribution of time since infection
       
+      %InfectionDate;                                                       %indicates the actual date of infection selected for this individual 
+      InfectionDateDistribution;                                            %indicates the actual date of infection selected for this individual
+      YearOfAIDSDiagnosis;
+      %YearTable;                                                           %DEPRECIATED
+      %YearFirstTreatment;                                                  %the year that patient goes on to treatment for the first time
+      IndigenousStatus;
       
+      %Variables used for Geographic Information
+      PostcodeAtDiagnosis;
+      StateAtDiagnosis;
+      SLAAtDiagnosis;
+      SRAtDiagnosis;
+      SSDAtDiagnosis;
       
+      %Variables that indicate recent diagnosis
+      DateIll;
+      DateIndetWesternBlot;
+      DateLastNegative;
       
-%       DateOfDeath;
-%       DateOfDeathContinuous;
-      YearOfDeath;%this is the only death flag (decimal year)
+      CD4CountAtDiagnosis;
+      ExposureRoute;
+      %PopulationCategory;                                                  %Category values
+      
+      %DateOfDeath;
+      %DateOfDeathContinuous;
+      YearOfDeath;                                                          %this is the only death flag (decimal year)
       PreviouslyDiagnosedOverseas;
       OverseasDiagnosisCountry;
       OverseasDiagnosisDate;
       RecentInfection;
       CountryOfBirth;
       
-
       HAARTStart;
       HAARTStop;
       
       %This variable is to indicate that it is simulated case or a case that has been actually diagnosed in real life
-      %Set to 0 if real case, 1 if simulated
-      SimulatedIndividual;
-      
-      
-      RegimenLine;%number of regimen that the patient is up to
-%       CD4CountArray;
-
-%       Condition;%an array of conditions that have corresponding numerical code. Days from start of simulation until condition appears
-%       Cancer;%an array of cancers that have corresponding numerical code. Days from start of simulation until condition appears
-      %Cancer(i).Year: year of cancer (i) diagnosis
-      %Cancer(i).Name:Name of Cancer
-      %Cancer(i).Code: code of cancer
-   end% properties
-
-   methods
-
-      function obj = PatientClass(obj)
-            obj.DateIll=NaN;
-            obj.DateIndetWesternBlot=NaN;
-            obj.DateLastNegative=NaN;
+      SimulatedIndividual;                                                  %Set to 0 if real case, 1 if simulated
+      RegimenLine;                                                          %number of regimen that the patient is up to
+      %CD4CountArray;      
+    end                                                                     %End of Properties
+    
+    methods
+        function obj = PatientClass(obj)
+            obj.DateIll = NaN;
+            obj.DateIndetWesternBlot = NaN;
+            obj.DateLastNegative = NaN;
+        end  
+   
+      function Age = CurrentAge(obj, Year)
+          Age = Year-obj.YearBirth;
       end
       
-
-%       function obj = set.Alive(obj, Alive)
-%          if Alive~=0 &&  Alive~=1
-%             error('Incorrect alive status entered (not 1 or 0)')
-%          end
-%          obj.Alive = Alive;
-%          
-%       end 
-%       
-% 
-%       function obj = set.HIVStatus(obj, HIVStatus)
-%          if HIVStatus~=0 &&  HIVStatus~=1
-%             error('Incorrect HIV status entered (not 1 or 0)')
-%          end
-%          obj.HIVStatus = HIVStatus;
-%       end 
-%       
-% 
-%       function obj = set.AIDSStatus(obj, AIDSStatus)
-%          if AIDSStatus~=0 &&  AIDSStatus~=1
-%             error('Incorrect AIDS status entered (not 1 or 0)')
-%          end
-%          obj.AIDSStatus = AIDSStatus;
-%       end 
-      
-
-      
-      
-      function Age=CurrentAge(obj, Year)
-          Age=Year-obj.YearBirth;
-      end
-      
-      function AliveAndHIV=AliveAndHIVPosInYear(obj, Year)
-          
-          %This is a vectorised form to allow fast processing
-          AliveAndHIV=obj.DateOfDiagnosisContinuous<=Year & Year<obj.YearOfDeath;
+      function AliveAndHIV = AliveAndHIVPosInYear(obj, Year)                %This is a vectorised to allow fast processing
+          AliveAndHIV = obj.DateOfDiagnosisContinuous<=Year&Year<obj.YearOfDeath;
           
       end
       
       function AIDSStatus = CurrentAIDSStatus(obj, CurrentYear)
-          if CurrentYear>=obj.YearOfAIDSDiagnosis
-              AIDSStatus=true;
+          if CurrentYear >= obj.YearOfAIDSDiagnosis
+              AIDSStatus = true;
           else
-              AIDSStatus=false;
+              AIDSStatus = false;
           end
       end
       
@@ -174,23 +128,20 @@ classdef PatientClass %< handle
       
       function obj = DetermineInfectionDateDistribution(obj)
 
-          obj.InfectionDateDistribution=obj.DateOfDiagnosisContinuous- obj.TimeFromInfectionToDiagnosis;
+          obj.InfectionDateDistribution = obj.DateOfDiagnosisContinuous - obj.TimeFromInfectionToDiagnosis;
       end
       
-      function ExpectedInfections = ExpectedInfectionsPriorTo(obj, Year)
-          %Gives the fractional (<1) amount of 
-          [~, SizeDistArray]=size(obj.InfectionDateDistribution);
-          ExpectedInfections=sum(obj.InfectionDateDistribution<Year)/SizeDistArray;
+      function ExpectedInfections = ExpectedInfectionsPriorTo(obj, Year)    %Gives the fractional (<1) amount
+          [~, SizeDistArray] = size(obj.InfectionDateDistribution);
+          ExpectedInfections = sum(obj.InfectionDateDistribution<Year) / SizeDistArray;
       end
       
-      function PlotDistributionOfTimes(obj)
-          % This function plots the distirbution of time between infection
-          % and diagnosis for this individual.
+      function PlotDistributionOfTimes(obj)                                 % This function plots the distirbution of time between infection and diagnosis for this individual.
             hist(obj.TimeFromInfectionToDiagnosis, 0.25:0.5:20);
             xlabel('Time until diagnosis (years)','fontsize', 18);
             ylabel('Probability density','fontsize', 18);
             set(gca,'Color',[1.0 1.0 1.0]);
-            set(gcf,'Color',[1.0 1.0 1.0]);%makes the grey border white
+            set(gcf,'Color',[1.0 1.0 1.0]);                                 %makes the grey border white
             set(gca, 'fontsize', 18)
             box off;
             
@@ -204,8 +155,29 @@ classdef PatientClass %< handle
             disp(median(obj.TimeFromInfectionToDiagnosis))
       end
       
-      
-      
-
-   end% methods
-end% classdef
+      %function obj = set.Alive(obj, Alive)
+%          if Alive~=0 &&  Alive~=1
+%             error('Incorrect alive status entered (not 1 or 0)')
+%          end
+%          obj.Alive = Alive;
+%          
+%       end 
+%       
+% 
+      %function obj = set.HIVStatus(obj, HIVStatus)
+%          if HIVStatus~=0 &&  HIVStatus~=1
+%             error('Incorrect HIV status entered (not 1 or 0)')
+%          end
+%          obj.HIVStatus = HIVStatus;
+%       end 
+%       
+% 
+      %function obj = set.AIDSStatus(obj, AIDSStatus)
+%          if AIDSStatus~=0 &&  AIDSStatus~=1
+%             error('Incorrect AIDS status entered (not 1 or 0)')
+%          end
+%          obj.AIDSStatus = AIDSStatus;
+%       end 
+    end                                                                     %End of Methods
+    
+end                                                                         %End of Class Definition
