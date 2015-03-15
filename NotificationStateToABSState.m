537303541ccbfd218dcd5627fa465387a37d22cc
hussainazfar <hussainazfar@gmail.com>
2015-03-15 17:10:19 +1100

    State Data Added into LineDataMatrix
    State Data converted into ABSState and stored into the LineDataMatrix
    for geographical processing

--- NotificationStateToABSState.m
+++ NotificationStateToABSState.m
@@ -1,46 +1,36 @@
-function [ABSState]=NotificationStateToABSState(NotificationState)
+function [ABSState] = NotificationStateToABSState(NotificationState)
 % 
 % State	Notification	ABS
-% NSW	2	1
-% VIC	7	2
-% QLD	4	3
-% SA	5	4
-% WA	8	5
-% TAS	6	6
-% NT	3	7
-% ACT	1	8
+% NSW       2           1
+% VIC       7           2
+% QLD       4           3   
+% SA        5           4
+% WA        8           5
+% TAS       6           6
+% NT        3           7
+% ACT       1           8
 
-StateMatrix=[8 1 7 3 4 6 2 5];
+%StateMatrix = [8 1 7 3 4 6 2 5];
 
-ABSState=StateMatrix(NotificationState);
+ABSState = [];
 
-% ABSState
-% 
-% if NotificationState==	2	
-%     ABSState=	1	;	
-% end
-% if NotificationState==	7	
-%     ABSState=	2	;	
-% end
-% if NotificationState==	4	
-%     ABSState=	3	;	
-% end
-% if NotificationState==	5	
-%     ABSState=	4	;	
-% end
-% if NotificationState==	8	
-%     ABSState=	5	;	
-% end
-% if NotificationState==	6	
-%     ABSState=	6	;	
-% end
-% if NotificationState==	3	
-%     ABSState=	7	;	
-% end
-% if NotificationState==	1	
-%     ABSState=	8	;	
-% end
-% 
-% ABSState
-
+    for x = 1:length(NotificationState)
+        if NotificationState ==	2	
+            ABSState(x) =	1;	
+        elseif NotificationState==	7	
+            ABSState(x) =	2;	
+        elseif NotificationState==	4	
+            ABSState(x) =	3;	
+        elseif NotificationState==	5	
+            ABSState(x) =	4;	
+        elseif NotificationState==	8	
+            ABSState(x) =	5;	
+        elseif NotificationState==	6	
+            ABSState(x) =	6;	
+        elseif NotificationState==	3	
+            ABSState(x) =	7;	
+        elseif NotificationState==	1	
+            ABSState(x) =	8;	
+        end
+    end
 end
