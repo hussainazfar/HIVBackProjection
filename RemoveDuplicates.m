3baeb311d6d333d5b4a98c4cbae7068d8e63c6d3
hussainazfar <hussainazfar@gmail.com>
2015-03-16 11:42:26 +1100

    RemoveDuplicate Function Integrated
    The RemoveDuplicate Function was updated an Integrated into
    BackProjection after variable naming normalisation with remaining code

--- RemoveDuplicates.m
+++ RemoveDuplicates.m
@@ -1,104 +1,98 @@
-function [PatientsIncluded, PatientsRemoved]=RemoveDuplicates(Patient)
+function [ PatientsIncluded, PatientsRemoved ] = RemoveDuplicates( Patient )
+%RemoveDuplicates.m checks the structure Patient for any duplicates that
+%might exist and removes recurrences
+%   The structure Patient is evaluated and duplicated data is removed from
+%   structure Patient, updated and returned to PatientsIncluded Structure.
+%   All Duplicate copies are stored in another structure Patients Removed.
 
+NoPatients = length(Patient);
 
-[~, NoPatients]=size(Patient);
+Duplicates = [];
+YearBirthAll = zeros(1, NoPatients);
+SexAll = zeros(1, NoPatients);
+DateDiagnosisAll = zeros(1, NoPatients);
 
-Duplicates=[];
-
-YearBirthAll=zeros(1, NoPatients);
-SexAll=zeros(1, NoPatients);
-DateDiagnosisAll=zeros(1, NoPatients);
-
-for i=1:NoPatients
-    YearBirthAll(i)=Patient(i).YearBirth;
-    SexAll(i)=Patient(i).Sex;
-    DateDiagnosisAll(i)= Patient(i).DateOfDiagnosisContinuous;
+for x = 1:NoPatients
+    YearBirthAll(x) = Patient(x).YearBirth;
+    SexAll(x) = Patient(x).Sex;
+    DateDiagnosisAll(x) = Patient(x).DateOfDiagnosisContinuous;
 end
 
-for SexUnderAnalysis=1:2
-    SexIndex=SexUnderAnalysis==SexAll;
+for SexUnderAnalysis = 1:2
+    SexIndex = SexUnderAnalysis == SexAll;
 
-    UniqueBirthYears=unique(YearBirthAll(SexIndex));%Used to avoid years in which there are no people born in that year which may cause problems later in the codes
+    UniqueBirthYears = unique(YearBirthAll(SexIndex));                      %Used to avoid years in which there are no people born in that year which may cause problems later in the codes
     
     %for each year in the data
-    for ThisBirthYear=UniqueBirthYears
+    for ThisBirthYear = UniqueBirthYears
         
-        IndicesForThisYear=[];
-        DOB={};
+        IndicesForThisYear = [];
+        DOB = [];
+        
         % Select patients with birthdates in the year
-        for i=1:NoPatients
-            if Patient(i).YearBirth==ThisBirthYear && Patient(i).Sex==SexUnderAnalysis
-                IndicesForThisYear=[IndicesForThisYear i];
-                DOB=[DOB Patient(i).DOB];
+        for x = 1:NoPatients
+            if Patient(x).YearBirth == ThisBirthYear && Patient(x).Sex == SexUnderAnalysis
+                IndicesForThisYear = [IndicesForThisYear x];
+                DOB = [DOB Patient(x).DOB];
             end
         end
         
-        if mod(ThisBirthYear, 4)==0 %leap year
-            PossibleDates=366;
+        if mod(ThisBirthYear, 4) == 0                                         %leap year
+            PossibleDates = 366;
         else
-            PossibleDates=365;
+            PossibleDates = 365;
         end    
             
-        UniqueDOBVector=unique(DOB);
-        [~, NoUniqueDates]=size(UniqueDOBVector);
+        UniqueDOBVector = unique(DOB);
+        NoUniqueDates = length(UniqueDOBVector);
 
-        [~, TotalRecordsInThisYear]=size(IndicesForThisYear);
-        NumberExpectedThisYear=PossibleDates*log(PossibleDates/(PossibleDates-NoUniqueDates));
-        NumberExpectedThisYear=round(NumberExpectedThisYear);
+        TotalRecordsInThisYear = length(IndicesForThisYear);
+        NumberExpectedThisYear = PossibleDates * log(PossibleDates/(PossibleDates-NoUniqueDates));
+        NumberExpectedThisYear = round(NumberExpectedThisYear);
         
-        disp([num2str(NumberExpectedThisYear) ' expected out of ' num2str(TotalRecordsInThisYear) ' in ' num2str(ThisBirthYear)]);
+        %disp([num2str(NumberExpectedThisYear) ' expected out of ' num2str(TotalRecordsInThisYear) ' in ' num2str(ThisBirthYear)]);
         
         
-        NumberConfirmed=0;
+        NumberConfirmed = 0;
         
-        ThisYearsDuplicateSample=[];
+        ThisYearsDuplicateSample = [];
         
         % For each date remove the first diagnosis on a date
-        for ThisDOB=UniqueDOBVector
+        for ThisDOB = UniqueDOBVector
             %Find records in this year with this DOB
-            PossibleDuplicatesOnThisDayIndex=strcmp(DOB,ThisDOB);
-            PossibleDuplicatesOnThisDay=IndicesForThisYear(PossibleDuplicatesOnThisDayIndex);
+            PossibleDuplicatesOnThisDayIndex = strcmp(DOB,ThisDOB);
+            PossibleDuplicatesOnThisDay = IndicesForThisYear(PossibleDuplicatesOnThisDayIndex);
             
             % Find the earliest diagnosis date
-            [~, MinIndex]=min(DateDiagnosisAll(PossibleDuplicatesOnThisDay));
+            [~, MinIndex ] = min(DateDiagnosisAll(PossibleDuplicatesOnThisDay));
             
             %Remove this as a possible duplicate from this 
-            PossibleDuplicatesOnThisDay(MinIndex)=[];
+            PossibleDuplicatesOnThisDay(MinIndex) = [];
             
-            ThisYearsDuplicateSample=[ThisYearsDuplicateSample  PossibleDuplicatesOnThisDay];
-            NumberConfirmed=NumberConfirmed+1;%note that this always adds 1 
+            ThisYearsDuplicateSample = [ThisYearsDuplicateSample  PossibleDuplicatesOnThisDay];
+            NumberConfirmed = NumberConfirmed + 1;                          %note that this always adds 1 
         end
         
-        NumberToRemove=TotalRecordsInThisYear-NumberExpectedThisYear;
+        NumberToRemove = TotalRecordsInThisYear - NumberExpectedThisYear;
          
         %select, at random, the duplicates
-        [~, NumberInSample]=size(ThisYearsDuplicateSample);
-        if NumberToRemove>=NumberInSample
-            Duplicates=[Duplicates ThisYearsDuplicateSample];
+        NumberInSample = length(ThisYearsDuplicateSample);
+        if NumberToRemove >= NumberInSample
+            Duplicates = [Duplicates ThisYearsDuplicateSample];
         else
-            Duplicates=[Duplicates randsample(ThisYearsDuplicateSample, NumberToRemove, false)];%replacement=false
+            Duplicates = [Duplicates randsample(ThisYearsDuplicateSample, NumberToRemove, false)];  %replacement=false
         end
         
     end
 end
 
+PatientsIncluded = Patient;
+PatientsIncluded(Duplicates) = [];
+PatientsRemoved = Patient(Duplicates);
 
-
-PatientsIncluded=Patient;
-PatientsIncluded(Duplicates)=[];
-PatientsRemoved=Patient(Duplicates);
-
-
-% 
-% subplot(1,3, 1);
-% hist(DateDiagnosisAll, 1980.5:2013.5)
-% 
-% subplot(1,3, 2);
-% hist(DateDiagnosisAll(Duplicates), 1980.5:2013.5)
-% 
-TotalDiagnoses=hist(DateDiagnosisAll, 1980.5:2013.5);
-NumDuplicates=hist(DateDiagnosisAll(Duplicates), 1980.5:2013.5);
-Prop=NumDuplicates./TotalDiagnoses;
+TotalDiagnoses = hist(DateDiagnosisAll, 1980.5:2013.5);
+NumDuplicates = hist(DateDiagnosisAll(Duplicates), 1980.5:2013.5);
+Prop = NumDuplicates./TotalDiagnoses;
 
 clf;
 bar(1980:2013, Prop*100);
